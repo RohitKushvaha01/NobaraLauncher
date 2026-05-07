3538e0437b7e4dcf8030c16a439655e97563a58e
Rohit Kushwaha <oldisg131@gmail.com>
2026-05-07 14:00:04 +0530

    fixed: minor issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import { StatusBar, useColorScheme } from 'react-native';
+import React, { useEffect } from 'react';
+import { BackHandler, StatusBar, useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppContent from './AppContent';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    const onBackPress = () => {
+      // Return true to stop the app from exiting
+      // You can add logic here (e.g., navigate to home screen)
+      return true; 
+    };
+
+    // Add the listener
+    const subscription = BackHandler.addEventListener(
+      'hardwareBackPress',
+      onBackPress
+    );
+
+    // Clean up the listener when the component unmounts
+    return () => subscription.remove();
+  }, []);
 
   return (
     <SafeAreaProvider>
       <StatusBar 
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'} 
         translucent 
         backgroundColor="transparent" 
       />
